67f1e93ab94ad11fe62fe954e7a164d6d7980c5c
Amanda Souza <85000013+asvsz@users.noreply.github.com>
2023-04-19 21:00:46 -0300

    Update questao3.py

--- questao3.py
+++ questao3.py
@@ -2,6 +2,6 @@ num = int(input('Digite um numero: '))
 
 if (num % 2 == 0):
     print(num)
-    print('O numero digitado é Par!')
+    print('O numero digitado e Par!')
 else:
-    print('O numero digitado é Impar!')
+    print('O numero digitado e Impar!')
